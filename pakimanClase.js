02e8b9f44f84f1fde432ac22bbd298095e12ccd0
Carlos Vega <carlosvegabello@gmail.com>
2021-04-05 09:00:06 -0400

    Segundo cambio, pakimanClase  y pakiman.html adicionando algunas palabras.

--- pakimanClase.js
+++ pakimanClase.js
@@ -18,8 +18,8 @@ class Pakiman
       document.body.appendChild(this.imagen);
       //document.write("<p>");
       document.write("<br /><strong>" + this.nombre + "</strong><br />");
-      document.write("Vida: " + this.vida + "<br />");
-      document.write("Ataque: " + this.ataque + "<hr />");
+      document.write("Vida!: " + this.vida + "<br />");
+      document.write("Ataque!: " + this.ataque + "<hr />");
       //document.write("</p>");
   }
 }
